57ab0e64874819b441a97fb441d2ae06871cc006
RonaldoCD <w.ronaldo.cd@gmail.com>
2023-02-13 20:26:38 -0500

    robot finished

--- my_robot_arm_description/launch/visualize.launch.py
+++ my_robot_arm_description/launch/visualize.launch.py
@@ -31,7 +31,6 @@ def generate_launch_description():
     # RVIZ Configuration
     rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', 'robot_arm.rviz')
 
-
     rviz_node = Node(
             package='rviz2',
             executable='rviz2',
